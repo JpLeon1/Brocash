f5091cf8b8e6e750720251635a442ec0aec0d931
JpLeon1 <juanpabloleonpineda@gmail.com>
2026-05-23 22:29:55 -0500

    Bases de Datos

---
+++ Database/brocash_registro_usuario.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: brocash
+-- ------------------------------------------------------
+-- Server version	8.0.46
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `registro_usuario`
+--
+
+DROP TABLE IF EXISTS `registro_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `registro_usuario` (
+  `ID_USUARIO` int NOT NULL,
+  `NOMBRE` varchar(30) DEFAULT NULL,
+  `APELLIDO` varchar(20) DEFAULT NULL,
+  `EDAD` int DEFAULT NULL,
+  `EMAIL` varchar(255) DEFAULT NULL,
+  `TELEFONO` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`ID_USUARIO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registro_usuario`
+--
+
+LOCK TABLES `registro_usuario` WRITE;
+/*!40000 ALTER TABLE `registro_usuario` DISABLE KEYS */;
+INSERT INTO `registro_usuario` VALUES (39548678,'Maria','Gomez',32,'maria@gmail.com','3109876543'),(1000394226,'Carlos','Ramirez',19,'carlos@outlook.com','3114567890'),(1014298336,'Juan','Leon',27,'juanpablo30_10@hotmail.com','3001234567');
+/*!40000 ALTER TABLE `registro_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-23 22:25:27
