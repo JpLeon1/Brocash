29bc5bb5564734a4ed796fccae4e2a98dfa109ca
JpLeon1 <juanpabloleonpineda@gmail.com>
2026-05-24 13:07:20 -0500

    Prueba de conexion base de datos

--- Database/Pruebadelapruebadelaprueba.sql
+++ Database/Pruebadelapruebadelaprueba.sql
@@ -1,1 +1,16 @@
-SELECT * FROM registro_usuario;
+INSERT INTO brocash.registro_usuario (
+    ID_USUARIO,
+    NOMBRE,
+    APELLIDO,
+    EDAD,
+    EMAIL,
+    TELEFONO
+)
+VALUES (
+    2,
+    'Maria',
+    'Gomez',
+    25,
+    'maria@test.com',
+    '3112223344'
+);
---
+++ src/servlets/conexion.java
@@ -0,0 +1,39 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/Brocash";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "6406";
+
+    public static Connection conectar() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD
+            );
+
+            System.out.println("Conexion exitosa");
+
+        } catch (Exception e) {
+
+            System.out.println("Error de conexion");
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
